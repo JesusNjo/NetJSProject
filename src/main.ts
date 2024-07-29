44265ac837fe183ba2ced10a06228454c18d7f8c
Francisco Naranjo <Naranjofjx@gmail.com>
2024-07-29 16:56:49 -0400

    port

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,8 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(process.env.PORT,'0.0.0.0');
+  console.log(port);
+  
 }
 bootstrap();
